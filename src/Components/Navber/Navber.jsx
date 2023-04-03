1bd8fd258ae811f2403b47d831385ce8b83569a5
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-04-03 14:31:27 +0600

    added useState

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
-import {Bars3Icon } from '@heroicons/react/24/solid'
+import {Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navber = () => {
+    const [open, setOpen] =useState(false);
     const routes = [
         { id: 1, name: 'Home', path: '/' },
         { id: 2, name: 'About', path: '/about' },
@@ -13,7 +14,9 @@ const Navber = () => {
       
     return (
         <nav>
-             <Bars3Icon className="h-6 w-6 text-purple-500" />
+           <div onClick={() => setOpen(!open)}>
+           <Bars3Icon className="h-6 w-6 text-purple-500" />
+           </div>
             <ul className='md:flex'>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
