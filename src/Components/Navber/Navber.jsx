fe2b3745ec2e2dff6ef09104b3eb46c9aeab97ac
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-04-03 14:38:19 +0600

    icon hidden from big device:

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 import Link from '../Link/Link';
-import {Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navber = () => {
-    const [open, setOpen] =useState(false);
+    const [open, setOpen] = useState(false);
     const routes = [
         { id: 1, name: 'Home', path: '/' },
         { id: 2, name: 'About', path: '/about' },
         { id: 3, name: 'Contact', path: '/contact' },
         { id: 4, name: 'Blog', path: '/blog' },
         { id: 5, name: 'Services', path: '/services' },
-      ];
-      
+    ];
+
     return (
         <nav>
-           <div onClick={() => setOpen(!open)}>
-           <Bars3Icon className="h-6 w-6 text-purple-500" />
-           </div>
+            <div onClick={() => setOpen(!open)} className='md:hidden'>
+                <span>{open === true ?
+                    <XMarkIcon className="h-6 w-6 text-purple-500" /> :
+                    <Bars3Icon className="h-6 w-6 text-purple-500" />}</span>
+
+
+            </div>
             <ul className='md:flex'>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
